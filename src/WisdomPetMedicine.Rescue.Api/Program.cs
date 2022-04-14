960d1a45b7939d9dbf872d4ec8ee9ada5b737b8c
Rodrigo Díaz Concha <rodrigo@rdiazconcha.com>
2022-04-14 00:08:41 -0500

    03_03e

--- src/WisdomPetMedicine.Rescue.Api/Program.cs
+++ src/WisdomPetMedicine.Rescue.Api/Program.cs
@@ -10,7 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRescueDb(builder.Configuration);
 builder.Services.AddScoped<AdopterApplicationService>();
 builder.Services.AddScoped<IRescueRepository, RescueRepository>();
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+                .AddDapr();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -27,6 +28,8 @@ if (app.Environment.IsDevelopment())
 app.EnsureRescueDbIsCreated();
 app.UseHttpsRedirection();
 app.UseAuthorization();
+app.UseCloudEvents();
+app.MapSubscribeHandler();
 app.MapControllers();
 
 app.Run();
